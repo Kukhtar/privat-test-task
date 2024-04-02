723507dff53cab4f3b87acfe02a8bee593764cdf
kukhtar <kukhtar27@gmail.com>
2024-04-02 11:18:47 +0300

    init

---
+++ src/main/java/com/kukhtar/privattesttask/PrivatTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.kukhtar.privattesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivatTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivatTestTaskApplication.class, args);
+	}
+
+}
